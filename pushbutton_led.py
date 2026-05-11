1e8a29135be595a22c5c9c80089026bf123668f6
SeoYoung Oh <myme0722@gachon.ac.kr>
2026-05-11 14:48:11 +0900

    add annotation

--- pushbutton_led.py
+++ pushbutton_led.py
@@ -1,10 +1,13 @@
 from gpiozero import Button, LED
 from signal import pause
 
+# Connect LED to GPIO pin 14
 led = LED(14)
+# Connect button to GPIO pin 4
 button = Button(4)
-
+# Turn LED on when button is pressed
 button.when_pressed = led.on
+# Turn LED off when button is released
 button.when_released = led.off
-
+# Keep program running
 pause()
